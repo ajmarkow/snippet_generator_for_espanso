e2a07689d04ab39f30a992f1c8e44ae9a8d39759
AJ Markow <alexanderjmarkow@gmail.com>
2024-06-13 10:43:42 -0500

    Add react json schema form component. Add material ui provider. Add import statements for both. Add Container in primary page layout so component does not fill entire page/viewport.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
-
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -10,8 +9,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <AppRouterCacheProvider>
     <html lang="en">
       <body className={inter.className}>{children}</body>
-    </html>
+      </html>
+    </AppRouterCacheProvider>
   );
 }
